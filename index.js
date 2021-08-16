57624f4c6ebafdd85d3b8a0dcf59aac265a7bed8
rukonit <rukon-it@live.com>
2021-08-15 23:46:56 -0400

    crud

--- index.js
+++ index.js
@@ -21,11 +21,38 @@ const movieSchema = new mongoose.Schema({
 
 const Movie = mongoose.model('Movie', movieSchema)
 
-const rukon = new Movie({
-    title: "Rukon",
-    year: 1990,
-    score: 9.5,
-    rating: "PG-13"
-})
+// const rukon = new Movie({
+//     title: "Rukon",
+//     year: 1990,
+//     score: 9.5,
+//     rating: "PG-13"
+// })
 
-rukon.save()
+// Movie.insertMany(
+//     [{
+//         title: "The Lion King",
+//         year: 2000,
+//         score: 5.5,
+//         rating: "PG-13"
+//     },
+//     {
+//         title: "Forrest Gump",
+//         year: 1995,
+//         score: 8.5,
+//         rating: "R"
+//     },
+//     {
+//         title: "Iron Man",
+//         year: 2000,
+//         score: 8.5,
+//         rating: "PG-13"
+//     },
+//     {
+//         title: "Zombi",
+//         year: 2015,
+//         score: 5.5,
+//         rating: "PG-13"
+//     }]
+
+
+// )
