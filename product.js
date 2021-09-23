d5e54d73aa71542e26f5d42e769f6b6fec48338f
rukonit <rukon-it@live.com>
2021-09-22 23:44:08 -0400

    model instance methods

--- product.js
+++ product.js
@@ -13,20 +13,49 @@ mongoose.connect('mongodb://localhost:27017/shopApp', {useNewUrlParser: true, us
 
 const productSchema = new mongoose.Schema({
     name: {type: String,
-    required: true},
+    required: true,
+    maxlenght: 20
+},
     price: {
-        type: Number
+        type: Number,
+        required: true,
+        min: [0, 'price must not be negative']
     },
+    onSale: {type: Boolean, 
+        default: false
+    },
+    size: {
+        type: String,
+        enum: ['S', 'M', 'L']
+    }
     })
+
+productSchema.methods.greet =  function() {
+    console.log(`Hello from ${this.name}`)
+}
+
+productSchema.methods.toggleSale =  function() {
+    this.onSale = !this.onSale;
+    return this.save();
+}
 
 const Product = mongoose.model('Product', productSchema)
 
-const bike = new Product({name: 'Mountain Bike', price: '1'})
-bike.save()
-.then(data=> {console.log("It Worked")
-console.log(data)
-})
-.catch(err => {
-    console.log("Oh no! error")
-    console.log(err)
-})
+const findProduct = async() => {
+    const foundProduct = await Product.findOne({name: 'Banana'});
+    console.log(foundProduct);
+    await foundProduct.toggleSale();
+    console.log(foundProduct);
+}
+
+findProduct()
+
+// const bike = new Product({name: 'Milk', price: '10', onSale: true, size: 'M'})
+// bike.save()
+// .then(data=> {console.log("It Worked")
+// console.log(data)
+// })
+// .catch(err => {
+//     console.log("Oh no! error")
+//     console.log(err)
+// })
